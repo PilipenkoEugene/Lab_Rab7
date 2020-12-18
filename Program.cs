7dce449c79cf455194078bc4dc12dc31fa291f31
PilipenkoEugene <70794926+PilipenkoEugene@users.noreply.github.com>
2020-12-18 19:57:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Евгений
+ * Date: 14.12.2020
+ * Time: 20:20
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+namespace BBB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif 
+            Sphere s = Sphere.createSphereFromFile();
+            //s.Load();
+            s.Info();
+
+#if !DEBUG 
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
---
+++ Sphere.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace BBB
+{
+    class Sphere
+    {
+        private double r;
+
+        private Sphere() {}
+        
+        public Sphere(double rr) {
+        	r = rr;
+        }
+        
+        public double getSquare() {
+        	return Math.PI * 4 * r * r;
+        }
+
+        public double getValue() { 
+        	return ((double) 4 / (double) 3) * Math.PI * Math.Pow(r, 3);
+        }
+        
+        public double getD() {
+        	return r * 2;
+        }
+
+        public void Info() {
+            String str = "Данные заданной сфере";
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("r = {0:0.00}", r));
+            Console.WriteLine(string.Format("d = {0:0.00}", getD()));
+            Console.WriteLine(string.Format("Vsp = {0:0.00}", getValue()));
+            Console.WriteLine(string.Format("Ssp = {0:0.00}", getSquare()));
+        }
+        
+        public void Info(ConsoleColor fg, ConsoleColor bgc) {
+        	Console.ForegroundColor = fg;
+        	Console.BackgroundColor = bgc;
+        	Console.Clear();
+        	Info();
+        }
+
+        public void Load() {
+            r = Convert.ToDouble(Console.ReadLine());
+        }
+        
+        public static Sphere createSphereFromFile(){
+        	double rFile = Convert.ToDouble(Console.ReadLine());
+        	return new Sphere(rFile);
+        }
+    }
+}
